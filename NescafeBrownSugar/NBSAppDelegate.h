9732526445ab84ce672410839fd618af5cd64928
Alexey Maydanik <Alexey.Maydanik@dataart.com>
2014-01-02 13:32:11 +0200

    created empty universal project

---
+++ NescafeBrownSugar/NBSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NBSAppDelegate.h
+//  NescafeBrownSugar
+//
+//  Created by Lexiren on 1/2/14.
+//  Copyright (c) 2014 COXO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NescafeBrownSugar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NescafeBrownSugar
+//
+//  Created by Lexiren on 1/2/14.
+//  Copyright (c) 2014 COXO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NBSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NBSAppDelegate class]));
+    }
+}
