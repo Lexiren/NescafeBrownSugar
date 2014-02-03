6442363f0884c80428470452bfcdf09ac7badd19
Alexey Maydanik <Alexey.Maydanik@dataart.com>
2014-02-03 21:00:48 +0200

    nescafe 3in1 brown sugar is on one line from now

--- NescafeBrownSugar/View Controllers/About Project/NBSAboutProjectViewController.m
+++ NescafeBrownSugar/View Controllers/About Project/NBSAboutProjectViewController.m
@@ -13,6 +13,7 @@
 NSString *const kNBSAboutProjectVCIdentifier = @"AboutProjectVC";
 
 @interface NBSAboutProjectViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *firstTextViewHeightConstraint;
 @property (nonatomic, weak) IBOutlet UITextView *firstTextView;
 @property (nonatomic, weak) IBOutlet UITextView *secondTextView;
 @property (nonatomic, weak) IBOutlet UILabel *pricesLabel;
@@ -23,11 +24,21 @@ NSString *const kNBSAboutProjectVCIdentifier = @"AboutProjectVC";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.firstTextView.text = @"Кайфуй від творчості та ділися з друзями натхненням в соціальних мережах. А додаток від \nNescafe 3в1 Brown Sugar — тобі в допомогу! \nЗаряди свій мозок для нових ідей!";
+    self.secondTextView.text = @"Щотижня отримуй нагоду стати власником фірмовoї футболки від Sekta з власним принтом та запас \nNescafe 3в1 Brown Sugar. Не втрачай шанс потішити себе! Стань фаном спiльнот Nescafe 3в1 та бери участь в акції! A ще – не забyдь долучати до голосування друзiв. \nБільше like – більше шансів на перемогу!";
     if (!NBS_isIPad) {
         CGFloat delta = NBS_IsDeviceScreenSize4InchOrBigger ? 0 : 2;
+        self.firstTextViewHeightConstraint.constant = (NBS_IsDeviceScreenSize4InchOrBigger) ? 140 : 100;
+        self.firstTextView.editable = YES;
+        self.secondTextView.editable = YES;
         self.firstTextView.font = [UIFont standartLightFontWithSize:16.f - delta];
         self.secondTextView.font = [UIFont standartLightFontWithSize:16.f - delta];
+        
+        self.firstTextView.editable = NO;
+        self.secondTextView.editable = NO;
         self.pricesLabel.font = [UIFont standartLightFontWithSize:22.f - delta];
+        
+        [self.view layoutIfNeeded];
     } else {
         [self.firstTextView replaceFontWithStandartLightFont];
         [self.secondTextView replaceFontWithStandartLightFont];
