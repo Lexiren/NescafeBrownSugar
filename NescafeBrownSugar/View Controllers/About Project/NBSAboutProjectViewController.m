c899916eb10a530a1c911eadcfa450630cee3ea5
Alexey Maydanik <Alexey.Maydanik@dataart.com>
2014-02-11 15:48:15 +0200

    changed about text

--- NescafeBrownSugar/View Controllers/About Project/NBSAboutProjectViewController.m
+++ NescafeBrownSugar/View Controllers/About Project/NBSAboutProjectViewController.m
@@ -24,15 +24,15 @@ NSString *const kNBSAboutProjectVCIdentifier = @"AboutProjectVC";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.firstTextView.text = @"Кайфуй від творчості та ділися з друзями натхненням в соціальних мережах. А додаток від \nNescafe 3в1 Brown Sugar — тобі в допомогу! \nЗаряди свій мозок для нових ідей!";
-    self.secondTextView.text = @"Щотижня отримуй нагоду стати власником фірмовoї футболки від Sekta з власним принтом та запас \nNescafe 3в1 Brown Sugar. Не втрачай шанс потішити себе! Стань фаном спiльнот Nescafe 3в1 та бери участь в акції! A ще – не забyдь долучати до голосування друзiв. \nБільше like – більше шансів на перемогу!\n\n*Apple Inc. не є організатором, спонсором, участником та ніяким iншим чином не пов'язана з діючою акцією, що проходить в мобільному додатку Nescafe 3в1 Brown Sugar";
+    self.firstTextView.text = @"Nescafe 3в1 Brown Sugar - модерновий мобільний додаток для створення власних шедеврів. З ним ти без проблем реалізуєш творчий потенціал!\nЯк він працює?\n- Встанови додаток\n- Авторизуйся через одну із соціальних мереж\n- Вибери малюнок, який хочеш відтвторити\n- Малюй натхненно\n- Ділися з друзями власним шедевром у соціальних мережах\n- Приєднайся до спільноти Nescafe 3в1 в Facebook чи ВКонтакті\n- Набирай більше лайків та отримуй подарунки від нового Nescafe 3в1 Brown Sugar\nЗаряджайся для нових ідей!\nДеталі на сайті: http://brown-sugar.com.ua/ \nЩотижня отримуй нагоду стати власником фірмовoї футболки від Sekta з власним принтом та запас \nNescafe 3в1 Brown Sugar. Не втрачай шанс потішити себе!";
+    self.secondTextView.text = @"*Apple Inc. не є організатором, спонсором, участником та ніяким iншим чином не пов'язана з діючою акцією, що проходить в мобільному додатку Nescafe 3в1 Brown Sugar";
     if (!NBS_isIPad) {
         CGFloat delta = NBS_IsDeviceScreenSize4InchOrBigger ? 0 : 2;
-        self.firstTextViewHeightConstraint.constant = (NBS_IsDeviceScreenSize4InchOrBigger) ? 100 : 70;
+        self.firstTextViewHeightConstraint.constant = (NBS_IsDeviceScreenSize4InchOrBigger) ? 340 : 260;
         self.firstTextView.editable = YES;
         self.secondTextView.editable = YES;
-        self.firstTextView.font = [UIFont standartLightFontWithSize:14.f - delta];
-        self.secondTextView.font = [UIFont standartLightFontWithSize:14.f - delta];
+        self.firstTextView.font = [UIFont standartLightFontWithSize:13.f - delta];
+        self.secondTextView.font = [UIFont standartLightFontWithSize:11.f - delta];
         
         self.firstTextView.editable = NO;
         self.secondTextView.editable = NO;
